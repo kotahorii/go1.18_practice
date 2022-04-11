7851ff6033f43c7791313d878fb3f02ffcde7320
kotahorii <khorii.micron@gmail.com>
2022-04-11 20:35:05 +0900

    initial commit

--- main.go
+++ main.go
@@ -6,16 +6,20 @@ import (
 )
 
 func main() {
-	fmt.Println(f([]MyInt{1, 2, 3, 4}))
+	s := New[string]()
+	s.Push("hello")
+	s.Push("hello")
+	fmt.Println(s.Pop())
+	fmt.Println(s.Pop())
 }
 
-func f[T Stringer](xs []T) []string {
-	var result []string
-	for _, x := range xs {
-		result = append(result, x.String())
-	}
-	return result
-}
+// func f[T Stringer](xs []T) []string {
+// 	var result []string
+// 	for _, x := range xs {
+// 		result = append(result, x.String())
+// 	}
+// 	return result
+// }
 
 type Stringer interface {
 	String() string
@@ -25,4 +29,21 @@ type MyInt int
 
 func (i MyInt) String() string {
 	return strconv.Itoa(int(i))
+}
+
+type Stack[T any] []T
+
+func New[T any]() *Stack[T] {
+	v := make(Stack[T], 0)
+	return &v
+}
+
+func (s *Stack[T]) Push(x T) {
+	(*s) = append((*s), x)
+}
+
+func (s *Stack[T]) Pop() T {
+	v := (*s)[len(*s)-1]
+	(*s) = (*s)[:len(*s)-1]
+	return v
 }

c1f0cbd9f4c97f1d9c974755062b3d5d8a75662f
kotahorii <khorii.micron@gmail.com>
2022-04-14 20:21:58 +0900

    initial commit

--- main.go
+++ main.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"fmt"
+
 	"golang.org/x/exp/constraints"
 )
 
 func main() {
-
+	cnt := 0
+	for i := 1; i <= 5; i++ {
+		for j := i + 1; j <= 5; j++ {
+			cnt++
+		}
+	}
+	fmt.Println(cnt)
 }
 
 type Stack[T constraints.Ordered] []T

d56e6b6a0837aa468fe937e185f162678a7b736f
kotahorii <khorii.micron@gmail.com>
2022-04-18 22:05:48 +0900

    initial commit

--- generics/knapsack.go
+++ generics/knapsack.go
@@ -1,13 +1,41 @@
 package generics
 
 import (
+	"fmt"
 	"math"
 	"sort"
 )
 
+func Make2DBoolArray(N, S int) [][]bool {
+	dp := make([][]bool, N+1)
+	for i := 0; i < N+1; i++ {
+		dp[i] = make([]bool, S+1)
+	}
+	dp[0][0] = true
+	for j := 1; j < S+1; j++ {
+		dp[0][j] = false
+	}
+	return dp
+}
+
+func Make2DIntArray(N, S int) [][]int {
+	dp := make([][]int, N+1)
+	for i := 0; i < N+1; i++ {
+		dp[i] = make([]int, S+1)
+	}
+	dp[0][0] = 0
+	for j := 1; j < S+1; j++ {
+		dp[0][j] = math.MinInt
+	}
+	return dp
+}
+
+// N, W := 4, 10
+// w := []int{3, 6, 4, 2}
+// v := []int{100, 210, 130, 57}
 func Knapsack(N, W int, w, v []int, dp [][]int) int {
-	for i := 1; i < N; i++ {
-		for j := 0; j < W; j++ {
+	for i := 1; i < N+1; i++ {
+		for j := 0; j < W+1; j++ {
 			if j < w[i-1] {
 				dp[i][j] = dp[i-1][j]
 			} else {
@@ -16,9 +44,29 @@ func Knapsack(N, W int, w, v []int, dp [][]int) int {
 			}
 		}
 	}
+	fmt.Println(dp)
 	max := func(a []int) int {
 		sort.Ints(a)
 		return a[len(a)-1]
-	}(dp[N-1])
+	}(dp[N])
 	return max
+}
+
+// a := []int{4, 1, 5}
+// N, S := 3, 10
+func SelectCards(N, S int, a []int, dp [][]bool) bool {
+	for i := 1; i < N+1; i++ {
+		for j := 0; j < S+1; j++ {
+			if j < a[i-1] {
+				dp[i][j] = dp[i-1][j]
+			} else {
+				if dp[i-1][j] || dp[i-1][j-a[i-1]] {
+					dp[i][j] = true
+				} else {
+					dp[i][j] = false
+				}
+			}
+		}
+	}
+	return dp[N][S]
 }
--- main.go
+++ main.go
@@ -1,23 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"math"
-
-	"go1.18/generics"
-)
-
 func main() {
-	N, W := 4, 10
-	w := []int{3, 6, 4, 2}
-	v := []int{100, 210, 130, 57}
-	dp := make([][]int, N)
-	for i := 0; i < N; i++ {
-		dp[i] = make([]int, W)
-	}
-	for i := 1; i < W; i++ {
-		dp[0][i] = math.MinInt
-	}
 
-	fmt.Println(generics.Knapsack(N, W, w, v, dp))
 }
